603cd8c5724c9365fedbf9be9a2ec3d2540da04b
DaTurret <mattcott14@gmail.com>
2020-04-09 15:45:46 +0200

    Corrected License Notice

--- src/mcctl/modules/service.py
+++ src/mcctl/modules/service.py
@@ -11,11 +11,11 @@
 # mcctl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY
 # without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with mcctl.  If not, see < http: // www.gnu.org/licenses/>.
+# along with mcctl. If not, see <http:// www.gnu.org/licenses/>.
 
 import shlex
 from pathlib import Path
