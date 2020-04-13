89ddf32eaf5e5e440d96aa6fc795dee1e2bab9e3
DaTurret <mattcott14@gmail.com>
2020-04-13 21:27:31 +0200

    Added License Notice

--- src/mcctl/__init__.py
+++ src/mcctl/__init__.py
@@ -1,4 +1,21 @@
-import sys
+# mcctl: A Minecraft Server Management Utility written in Python
+# Copyright (C) 2020 Matthias Cotting
+
+# This file is part of mcctl.
+
+# mcctl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# mcctl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with mcctl. If not, see <http://www.gnu.org/licenses/>.
+
 from mcctl import config, proc, service, status, storage, web, common
 
 __version__ = "0.1.0"
